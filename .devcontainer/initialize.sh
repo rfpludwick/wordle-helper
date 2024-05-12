12a745afc1f95102a018bcafe070f16b6df5d6bf
Robert F.P. Ludwick <robert@ludwick.us>
2024-05-11 19:20:13 -0700

    Launch.json

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# shellcheck disable=SC2154
-git config --global --add safe.directory "${containerWorkspaceFolder}"
-
 # Local initializer?
 if [ -x .devcontainer/initialize-local.sh ]; then
 	.devcontainer/initialize-local.sh
